749f1114b8e1a9e6f5567f89f9792cf7856c8e71
alex-held <git@alexheld.io>
2021-05-29 21:48:09 +0200

    test(sort): sort ranges by start value

--- pkg/merge/merge.go
+++ pkg/merge/merge.go
@@ -1,5 +1,37 @@
 package merge
 
-func Merge(in [][]int) (result [][]int) {
-	return in
+import (
+	"sort"
+)
+
+// Range is an interval with a start integer and a end integer.
+// It `always` consists of exactly 2 integers.
+type Range [2]int
+
+// Ranges is an interval of Range
+type Ranges []Range
+
+func (s Ranges) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s Ranges) Less(i, j int) bool {
+	return s[i][0] < s[j][0]
+}
+
+func (s Ranges) Len() int {
+	return len(s)
+}
+
+// Merge returns Ranges where overlapping Range values have been merged
+func Merge(ranges Ranges) (result Ranges) {
+	// sort the ranges by the starting value
+	sort.Sort(ranges)
+
+	// early return if the input is empty
+	if len(ranges) == 0 {
+		return ranges
+	}
+
+	return result
 }
--- pkg/merge/merge_test.go
+++ pkg/merge/merge_test.go
@@ -1,32 +1,51 @@
 package merge_test
 
 import (
+	"sort"
+
 	. "github.com/onsi/ginkgo"
+	"github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
 
 	. "github.com/alex-held/daimler-merge/pkg/merge"
 )
 
+var _ = Describe("Ranges", func() {
+	Context("sort.Sort(ranges)", func() {
+		table.DescribeTable(
+			"are sorted by ranges start value, ASC",
+			func(in, expected Ranges) {
+				sort.Sort(in)
+				Expect(in).Should(Equal(expected))
+			},
+			table.Entry("most negative left", Ranges{{-1, 5}, {-20, -10}}, Ranges{{-20, -10}, {-1, 5}}),
+			table.Entry("lesser left", Ranges{{1, 1}, {0, 1}}, Ranges{{0, 1}, {1, 1}}),
+			table.Entry("already ordered", Ranges{{1, 5}, {8, 10}}, Ranges{{1, 5}, {8, 10}}),
+			table.Entry("order overlap", Ranges{{3, 6}, {1, 5}}, Ranges{{-1, 5}, {3, 6}}),
+		)
+	})
+})
+
 var _ = Describe("merge intervals", func() {
 	Context("merge.Merge()", func() {
 		When("input is empty", func() {
 			It("returns empty interval", func() {
-				Expect(Merge([][]int{})).Should(BeEmpty())
+				Expect(Merge(Ranges{})).Should(BeEmpty())
 			})
 		})
 		When("input contains `negative` mergable intervals", func() {
 			It("returns intervals with merged intervals", func() {
-				Expect(Merge([][]int{{-1, 5}, {4, 10}, {-20, -10}})).Should(Equal([][]int{{-20, -10}, {-1, 10}}))
+				Expect(Merge(Ranges{{-1, 5}, {4, 10}, {-20, -10}})).Should(Equal(Ranges{{-20, -10}, {-1, 10}}))
 			})
 		})
 		When("input contains mergable intervals", func() {
 			It("returns intervals with merged intervals", func() {
-				Expect(Merge([][]int{{25, 30}, {2, 19}, {14, 23}, {4, 8}})).Should(Equal([][]int{{2, 23}, {25, 30}}))
+				Expect(Merge(Ranges{{25, 30}, {2, 19}, {14, 23}, {4, 8}})).Should(Equal(Ranges{{2, 23}, {25, 30}}))
 			})
 		})
 		When("input contains no mergable intervals", func() {
 			It("returns input without merged intervals", func() {
-				Expect(Merge([][]int{{1, 3}, {4, 6}, {7, 9}})).Should(Equal([][]int{{1, 3}, {4, 6}, {7, 9}}))
+				Expect(Merge(Ranges{{1, 3}, {4, 6}, {7, 9}})).Should(Equal(Ranges{{1, 3}, {4, 6}, {7, 9}}))
 			})
 		})
 	})
